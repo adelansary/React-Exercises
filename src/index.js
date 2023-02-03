21c0abdba8944a88742161effd8dc237b39a46e1
Adel A. Ansari <97963847+adelansary@users.noreply.github.com>
2023-02-03 08:11:00 +0800

    Components03

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { Hello } from "./Hello";
 import ReactDOM from "react-dom";
+import {App} from "./App";
 
-const hello = <Hello />;
+const app = <App />
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(hello);
+root.render(app);
