f6940234f9242648db8274d1975b84f978598148
Adel A. Ansari <97963847+adelansary@users.noreply.github.com>
2023-02-03 08:00:40 +0800

    Components01

--- src/Sum.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export class Sum extends React.Component {
-  render() {
-    const { a, b } = this.props;
-    return <h2>{a + b}</h2>;
-  }
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
-
-const app = <App />;
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(app);
