9fc16f41c338173aae29ea762dec1a7d60ab4031
Adel A. Ansari <97963847+adelansary@users.noreply.github.com>
2023-02-03 08:22:00 +0800

    Props01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import {Hello} from "./Hello";
+import { Hello } from "./Hello";
+import { Welcome } from "./Welcome";
 
-export class App extends React.Component{
-    render(){
-        return (
-        <div>
-            <Hello /> 
-        </div>
-        )
-    }
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Welcome name="Adel" />
+      </div>
+    );
+  }
 }
