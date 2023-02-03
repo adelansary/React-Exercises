937247ad2fe5793d5c3bf8da22e289a7e12bc836
Adel A. Ansari <97963847+adelansary@users.noreply.github.com>
2023-02-03 21:51:02 +0800

    Props02

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-export class Welcome extends React.Component{
-    render(){
-        return <p>Welcome, {this.props.name}</p>
-    }
+export class Welcome extends React.Component {
+  render() {
+    return <p>Welcome, {this.props.name}</p>;
+  }
 }
+
+Welcome.defaultProps = {
+  name: "Adel",
+};
