a197ba189e537481fd8a71dd592e9d00e83d61f4
Adel A. Ansari <97963847+adelansary@users.noreply.github.com>
2023-02-03 23:33:21 +0800

    Forms03

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,65 @@
 import React from "react";
 
-export class Login extends React.Component{
+export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    remember: false,
+    disable: true,
+  };
 
-    state = {
-        username : "",
-        password : "",
-        remember: false
-    }
+  inputEventHandlers = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    const type = event.target.type;
+    const checked = event.target.checked;
 
-    inputEventHandlers = (event) => {
-        const name = event.target.name
-        const value = event.target.value
-        const type = event.target.type
-        const checked = event.target.checked
-
+    this.setState(
+      {
+        [name]: type === "checkbox" ? checked : value,
+      },
+      () =>
         this.setState({
-            [name]: type === 'checkbox' ? checked : value
+          disable:
+            this.state.password !== "" && this.state.username !== ""
+              ? false
+              : true,
         })
-    }
+    );
+  };
 
-    render(){
-        return (
-            <div>
-                <input name="username" onChange={this.inputEventHandlers} value={this.state.username} type="username" /><br></br>
-                <input name="password" onChange={this.inputEventHandlers} value={this.state.password} type="password" /><br></br>
-                <input name="remember" onChange={this.inputEventHandlers} checked={this.state.remember} type="checkbox" /><br></br>
-            </div>                
-        )
-    }
+  callingOnLogin = () => {
+    this.props.passingFunction(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          name="username"
+          onChange={this.inputEventHandlers}
+          value={this.state.username}
+          type="username"
+        />
+        <br></br>
+        <input
+          name="password"
+          onChange={this.inputEventHandlers}
+          value={this.state.password}
+          type="password"
+        />
+        <br></br>
+        <input
+          name="remember"
+          onChange={this.inputEventHandlers}
+          checked={this.state.remember}
+          type="checkbox"
+        />
+        <br></br>
+        <button onClick={this.callingOnLogin} name="button" disabled={this.state.disable}>
+          Login
+        </button>
+      </div>
+    );
+  }
 }
