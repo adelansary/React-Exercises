524238978763a73d60caf3574ee5295ba9b147d6
Adel A. Ansari <97963847+adelansary@users.noreply.github.com>
2023-02-03 22:34:19 +0800

    Conditional rendering04

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <Welcome name={<strong>Ahmad</strong>} />
+        <Welcome name="Abed" />
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -4,13 +4,12 @@ import { Age } from "./Age";
 export class Welcome extends React.Component {
   static defaultProps = {
     name: "Adel",
-    age: "not specified",
   };
   render() {
     return (
       <div>
         <p>Welcome, {this.props.name}</p>
-        <Age age={19} />
+        {this.props.name === "Abed" && <Age age={25} />}
       </div>
     );
   }
